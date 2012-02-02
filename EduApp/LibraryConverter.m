96db67775962f983956fd1510d7b89e49445043c
Mikkel Gravgaard <mikkel@klokke.dk>
2012-02-02 12:14:09 +0100

    convertion stub

---
+++ EduApp/LibraryConverter.m
@@ -0,0 +1,19 @@
+//
+//  LibraryConverter.m
+//  EduApp
+//
+//  Created by Mikkel Gravgaard on 02/02/12.
+//  Copyright (c) 2012 Betafunk. All rights reserved.
+//
+
+#import "LibraryConverter.h"
+
+@implementation LibraryConverter
+@synthesize delegate;
+
+- (void)convert:(MPMediaItem *)item
+{
+    
+}
+
+@end
